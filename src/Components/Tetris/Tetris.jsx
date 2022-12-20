d044b4f674f16b106931350757d72f950452de0e
Podran93 <Podran93>
2022-12-20 08:09:32 +0100

    Feat: Create useGameStats Hook

--- src/Components/Tetris/Tetris.jsx
+++ src/Components/Tetris/Tetris.jsx
@@ -2,8 +2,11 @@ import "./Tetris.css";
 import Board from "../Board/Board";
 import  useBoard  from "../../Hooks/useBoard"
 import GameStats from "../GameStats/GameStats"
+import useGameStats from "../../Hooks/useGameStats"
 
 const Tetris = ({rows, columns, setGameOver}) => {
+    //Custom Hook gameStats
+    const [gameStats, addLinesCleared] = useGameStats();
     //Custom hook useBoard that takes rows and columns
     const [board, setBoard] = useBoard({rows, columns});
 
